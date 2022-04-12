c8262ae68d69e488b63ef98c95525da32c3fc8c2
ashuacharya <ashuacharya622@gmail.com>
2022-04-12 15:53:35 +0545

    doing file structure

--- src/components/Hero_Section/hero.jsx
+++ src/components/Hero_Section/hero.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import logo from "../../assets/Logo.svg";
 
 const hero = () => {
   return (
     <section className="hero__container">
       <div className="hero__content">
-        <div className="hero__content__nav"></div>
+        <div className="hero__content__nav">
+          <img src={logo} alt="Logo" />
+        </div>
         <div className="hero__content__middle">
           <h1 className="hero__content__heading"></h1>
           <h3 className="hero__content__subheading"></h3>
