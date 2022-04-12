e9c13cb039bbb08202623425245df73df88c7c80
ashuacharya <ashuacharya622@gmail.com>
2022-04-12 15:50:22 +0545

    hero section started

---
+++ src/components/Hero_Section/hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const hero = () => {
+  return (
+    <section className="hero__container">
+      <div className="hero__content">
+        <div className="hero__content__nav"></div>
+        <div className="hero__content__middle">
+          <h1 className="hero__content__heading"></h1>
+          <h3 className="hero__content__subheading"></h3>
+          <h3 className="hero__content__subheading"></h3>
+          <div className="hero__content__smallPlanets"></div>
+          <div className="hero__content__btn"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default hero;
